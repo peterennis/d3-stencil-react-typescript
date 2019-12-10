0aa3082894cbb1705f62ac9adec6643e00c65fcb
peterennis <peterennis@yahoo.com>
2019-12-09 20:47:24 -0800

    chore(deps): bump, use ae-d3-stencil from npm for testing, use stencil version of typescript

--- src/register-web-components.ts
+++ src/register-web-components.ts
@@ -3,7 +3,7 @@
 
 // Ref: WORKAROUND - https://github.com/ionic-team/stencil/issues/1636
 
-import { defineCustomElements, Components, JSX as LocalJSX } from 'd3-stencil/dist/loader';
+import { defineCustomElements, Components, JSX as LocalJSX } from 'ae-d3-stencil/dist/loader';
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
 
 type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
@@ -17,11 +17,11 @@ type ReactProps<T> = {
 type StencilToReact<
   T = LocalJSX.IntrinsicElements,
   U = HTMLElementTagNameMap
-> = StencilProps<T> & ReactProps<U>;
+  > = StencilProps<T> & ReactProps<U>;
 
 declare global {
   export namespace JSX {
-    interface IntrinsicElements extends StencilToReact {}
+    interface IntrinsicElements extends StencilToReact { }
   }
 }
 defineCustomElements(window);
